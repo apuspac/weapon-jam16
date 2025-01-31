afb9ff789210edc0503809bafc16a25d37d07664
apuspac <21313205+apuspac@users.noreply.github.com>
2025-01-31 23:03:56 +0900

    #37 手直しして提出

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -49,7 +49,7 @@ func stage_cleared():
     var next_stage: String = "stage" + str(next_stage_int) + ".tscn"
 
     # 10ステージだった場合 endに遷移
-    if next_stage_int > 8:
+    if next_stage_int > 10:
         get_tree().change_scene_to_file("res://scenes/end.tscn")
     else:
         get_tree().change_scene_to_file("res://scenes/" + next_stage)
