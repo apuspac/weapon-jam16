1e76cffdc2aa80a24a150d0077c4c5613320be93
apuspac <21313205+apuspac@users.noreply.github.com>
2025-01-31 11:45:42 +0900

    #30 stage9 隠れる足場の縦だけの実装, #31 の修正

--- scripts/parts_scripts/enemy_bullet_floor.gd
+++ scripts/parts_scripts/enemy_bullet_floor.gd
@@ -1,11 +1,15 @@
 extends Enemy
 
 var enemy_floor
+var animation_floor
 
 func _ready():
     super._ready()
     enemy_floor = get_node("MoveFloor")
+    animation_floor = get_node("../AnimationPlayer")
+    animation_floor.play("RESET")
+    animation_floor.play("move")
 
 func _on_enemy_area_body_entered(_body: Node2D):
     super._on_enemy_area_body_entered(_body)
-    enemy_floor.visible = false
+    enemy_floor.queue_free()
